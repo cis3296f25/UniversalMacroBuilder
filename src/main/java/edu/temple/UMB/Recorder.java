266ccb7b069ec0fe5b70bb096cdb0f4eedcc342f
Temitope Raymond <tup80767@temple.edu>
2025-10-29 15:29:57 -0400

    writer function implemeneted into recorder

--- src/main/java/edu/temple/UMB/Recorder.java
+++ src/main/java/edu/temple/UMB/Recorder.java
@@ -1,8 +1,6 @@
 package edu.temple.UMB;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Recorder {
     private final File outPath;
@@ -24,27 +22,15 @@ public class Recorder {
             }
 
             System.out.println("Recording stopped, saving file...");
-            writeToFile();
+
+            Writer writer = new Writer(Writer.Type.KEY);
+            writer.writeToFile(outPath, keyboardRecorder.getEvents());
+
+            System.out.println("Saved recorded keys to: " + outPath.getAbsolutePath());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    private void writeToFile() {
-        try (FileWriter writer = new FileWriter(outPath)) {
-            writer.write("START KEY EVENTS\n");
-
-            for (KeyEvent event : keyboardRecorder.getEvents()) {
-                long delta = event.getDelta(); // already time since first key
-                String keyText = com.github.kwhat.jnativehook.keyboard.NativeKeyEvent.getKeyText(
-                        event.getEvent().getKeyCode());
-                writer.write(delta + " KEY_PRESSED " + keyText + "\n");
-            }
-
-            writer.write("END KEY EVENTS\nEOF\n");
-            System.out.println("Saved recorded keys to: " + outPath.getAbsolutePath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
+
