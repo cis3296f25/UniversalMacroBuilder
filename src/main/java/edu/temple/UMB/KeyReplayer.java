5ccde74aef93e3bc48cac2f41ffb1c299c58cd14
Julia Fasick <juli4fasick@gmail.com>
2025-11-03 14:28:32 -0500

    finished a (messy) implementation

--- src/main/java/edu/temple/UMB/KeyReplayer.java
+++ src/main/java/edu/temple/UMB/KeyReplayer.java
@@ -1,12 +1,42 @@
 package edu.temple.UMB;
 
+import java.awt.*;
 import java.util.LinkedHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class KeyReplayer {
-    LinkedHashMap<Long, Integer> awtEvents = new LinkedHashMap<>();
+    LinkedHashMap<Long, AWTReplayEvent> awtEvents;
+    public final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final long startTime = System.currentTimeMillis();
+    private Robot robot;
 
-    public KeyReplayer(LinkedHashMap<Long, Integer> awtEvents) {
+
+    public KeyReplayer(LinkedHashMap<Long, AWTReplayEvent> awtEvents) {
         this.awtEvents = awtEvents;
+    }
+
+    public void start() {
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+        for (Long key : awtEvents.keySet()) {
+            long delay = key - (System.currentTimeMillis() - startTime);
+            if (delay < 0) delay = 0; // skip past events
+            scheduler.schedule(() -> executeEvent(awtEvents.get(key)), delay, TimeUnit.MILLISECONDS);
+        }
+    }
+
+    private void executeEvent(AWTReplayEvent event) {
+        System.out.println("Executing " + event.context + " with code " + event.event);
+        if (event.context.equals("PRESSED")) {
+            robot.keyPress(event.event);
+        } else if (event.context.equals("RELEASED")) {
+            robot.keyRelease(event.event);
+        }
     }
 }
 
