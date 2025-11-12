7317bf519239fb0dcf226769e4638dc205d90f44
Julia Fasick <juli4fasick@gmail.com>
2025-11-12 13:35:38 -0500

    adds logger functionality to other classes

--- src/main/java/edu/temple/UMB/Writer.java
+++ src/main/java/edu/temple/UMB/Writer.java
@@ -2,8 +2,11 @@ package edu.temple.UMB;
 
 import java.io.*;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Writer {
+    private static final Logger logger = LogManager.getLogger(Writer.class);
     public enum Type { MOUSE, KEY }
 
     private final Type type;
@@ -13,6 +16,7 @@ public class Writer {
     }
 
     public void writeToFile(File path, List<? extends Event> events) throws IOException {
+        logger.info("Writing {} {} events to {}", events.size(), type.name(), path.getAbsolutePath());
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true))) {
             writer.write("START " + type.name() + " EVENTS\n");
             for (Event e : events) {
@@ -23,7 +27,10 @@ public class Writer {
             if(type.name().equals("MOUSE")){
                 writer.write("EOF\n");
             }
-
+            logger.debug("Finished writing {} events of type {}", events.size(), type.name());
+        } catch (IOException ex) {
+            logger.error("Failed to write {} events of type {} to {}", events.size(), type.name(), path.getAbsolutePath(), ex);
+            throw ex;
         }
     }
 }
